fd6de985e578a2e15e1ef75d0e586eeae9ccfc07
oliverhilstrom <819492@gmail.com>
2021-10-06 14:59:00 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    x = 1
+    for i in range(0, len(w)):
+        if w[i] == "a":
+            return len(w[i])
+        else:
+            return 0
+        x = x + 1
+print(countA("bench"))
+
