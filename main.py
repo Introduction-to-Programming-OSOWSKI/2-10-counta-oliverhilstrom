ebea094addd5d8c6b275cfab2dfa07043f17f9a9
oliverhilstrom <819492@gmail.com>
2021-10-06 15:15:26 -0500

    2

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
 def countA(w):
-    x = 1
+    x = 0
     for i in range(0, len(w)):
         if w[i] == "a":
-            return len(w[i])
-        else:
-            return 0
-        x = x + 1
-print(countA("bench"))
+            x = x + 1
+    return x 
+print(countA("alabama"))
 
